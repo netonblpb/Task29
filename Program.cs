987dd6f83b6e0e62a2fa6781d4e7cf95986c7ccf
netonblpb <skullbog@gmail.com>
2022-02-11 15:37:48 +0300

    Task29 complete

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿//Написать программу вычисления произведения чисел от 1 до N
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+int fact = 1;
+int i = 1;
+while (i <= N)
+{
+    fact *= i;
+    i++;
+}
+
+Console.WriteLine($"Произведение чисел от 1 до {N}: {fact}");
